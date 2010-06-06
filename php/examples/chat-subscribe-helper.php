8ed2ef1f0d6f325fa2a6a4a0d92b93c602a6a0b7
Stephen L. Blum <blum.stephen@gmail.com>
2010-06-05 22:21:27 -0700

    added PHP Command Line Chat in the Pubnub API Examples.

--- php/examples/chat-subscribe-helper.php
+++ php/examples/chat-subscribe-helper.php
@@ -4,40 +4,38 @@
     $publish_key   = isset($argv[1]) ? $argv[1] : false;
     $subscribe_key = isset($argv[2]) ? $argv[2] : false;
 
-    # Print usage if missing info.
-    if (!($publish_key && $subscribe_key)) {
-echo("
-    ==============
-    EXAMPLE USAGE:
-    ==============
-    php ./subscribe-example.php PUBLISH-KEY SUBSCRIBE-KEY
-
-");
-        exit();
-    }
-
     ## Require Pubnub API
-    echo("Loading Pubnub.php Class\n");
     require('../Pubnub.php');
 
     ## -----------------------------------------
     ## Create Pubnub Client API (INITIALIZATION)
     ## -----------------------------------------
-    echo("Creating new Pubnub Client API\n");
     $pubnub = new Pubnub( $publish_key, $subscribe_key );
 
     ## ----------------------
     ## Send Message (PUBLISH)
     ## ----------------------
-    echo("Listening for Messages (Press ^C to stop)\n");
     $pubnub->subscribe(array(
-        'channel'  => 'hello_world',        ## REQUIRED Channel to Listen
+        'channel'  => 'php_chat',           ## REQUIRED Channel to Listen
         'callback' => function($response) { ## REQUIRED Callback With Response
-            var_dump($response); ## Print Response
-            return true;         ## Keep listening (return false to stop)
+            ## Capture Message Data
+            $message = $response['messages'][0];
+
+            ## Print Message
+            echo(
+                "["              .
+                date('H:i:s')    .
+                "] <"            .
+                $message['from'] .
+                "> "             .
+                $message['text'] .
+                "\r\n"
+            );
+
+            ## Keep listening (return false to stop)
+            return true;
         }
     ));
-
 
 ?>
 
