7983cacc673b9e95236cde8e8891bf1af67fb14c
Stephen L. Blum <blum.stephen@gmail.com>
2010-12-17 19:50:28 -0800

    added new PubNub Perl 5 Real-time Cloud Push API provided by CompletelyPrivateFiles.com website.

---
+++ perl5/Pubnub.pm
@@ -0,0 +1,147 @@
+package Pubnub;
+
+use Modern::Perl;
+use LWP::Simple;
+use URI::Escape;
+use JSON;
+use Digest::MD5 qw(md5_hsx);
+
+our $origin = 'pubsub.pubnub.com';
+our $limit = 1800;
+our $TRACE = 0;
+
+sub new {
+
+    my $class = shift;
+    my $self = {};
+    
+    $self->{'pubkey'} = shift;
+    $self->{'subkey'} = shift;
+    $self->{'secret'} = 0;
+    $self->{'ssl'} = shift;
+
+    # default to 'demo'
+    $self->{'pubkey'} = $self->{'pubkey'} ? $self->{'pubkey'} : 'demo';
+    $self->{'subkey'} = $self->{'subkey'} ? $self->{'subkey'} : 'demo';
+    
+    $self->{'protocol'} = $self->{'ssl'} ? 'https' : 'http';
+
+    bless($self, $class);
+    return $self;
+        
+} # new
+
+
+sub publish {
+    
+    my $self = shift;
+    my $h = shift;
+
+    die "Required params: \$channel, \$message\n" if 
+        !$h->{'channel'} || !$h->{'message'};
+
+    # handle double-quotes in message
+    my $esc_msg = $h->{'message'};
+    $esc_msg =~ s/"/\\"/g;
+
+    my $url = $self->{'protocol'} . '://' . $origin .
+        '/publish' . 
+        '/' . $self->{'pubkey'} .
+        '/' . $self->{'subkey'} .
+        '/0' .                  # TODO sig
+        '/' . $h->{'channel'} .
+        '/0' .                  # TODO callback 
+        '/' . uri_escape('"' . $esc_msg . '"' );
+
+    if (length($h->{'message'}) > $limit) {
+        return "[0, 'Message too long.']";
+    }
+
+    print "\n\n$url\n\n" if $TRACE;
+
+    return get($url);
+    
+} # publish
+
+
+# This is blocking
+sub subscribe {
+    
+    my $self = shift;
+    my $h = shift;
+
+    die "Required params: \$channel, \$callback\n" if 
+        !$h->{'channel'} || !$h->{'callback'};
+    
+    # shorteners
+    my $callback = $h->{'callback'};
+    my $timetoken = $h->{'timetoken'} ? $h->{'timetoken'} : '0';
+
+    # build URL from params
+    my $url = $self->{'protocol'} . '://' . $origin .
+        '/subscribe' . 
+        '/' . $self->{'subkey'} .
+        '/' . $h->{'channel'} .
+        '/0' .                  # TODO callback 
+        '/' . $timetoken;
+   
+    print "\n\n$url\n\n" if $TRACE;
+    print "$callback\n" if $TRACE; 
+
+    my $resp = decode_json(get($url));
+    my $messages = $$resp[0];
+
+    # timeout
+    if (!@$messages) { return $self->subscribe($h); }
+   
+    foreach my $msg (@$messages) {
+        return if !&$callback($msg); # user cancels
+    }
+    
+    return $self->subscribe($h);
+
+} # subscribe
+
+
+sub history {
+    
+    my $self = shift;
+    my $h = shift;
+
+    die "Required params: \$channel\n" if !$h->{'channel'};
+
+    my $total = $h->{'limit'} ? $h->{'limit'} : 10;
+
+    my $url = $self->{'protocol'} . '://' . $origin .
+        '/history' .
+        '/' . $self->{'subkey'} .
+        '/' . $h->{'channel'} .
+        '/0' .                  # TODO callback
+        '/' . $total;
+
+    return get($url);
+
+} # history
+
+1;
+
+__END__
+Copyright (c) 2010 CompletelyPrivateFiles.com, LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ perl5/demo.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+use threads;
+use threads::shared;
+use Pubnub;
+
+my $channel = 'some_unique_channel_perhaps';
+
+my $p = Pubnub->new(); # defaults to 'demo' channel
+
+
+# call these on thread, since subscribe() will block
+my $th = threads->create('subscribe');
+my $th2 = threads->create('publish');
+my $th3 = threads->create('history');
+$th->join();
+$th2->join();
+$th3->join();
+
+
+# called by threads:
+
+sub publish {
+    sleep(1); # give subscribe() a head start
+    print 'publish(): ',
+        $p->publish( { 
+            channel=> $channel, 
+            message=> 'He said, "helowrld"' 
+        }), 
+        "\n";
+}
+
+sub subscribe {
+    print "Started subscribe thread...\n";
+    $p->subscribe( {
+            channel => $channel, 
+            callback => 
+                sub { 
+                    my $msg = shift; 
+                    print "subscribe(): $msg\n"; 
+                    return 0; # stop after one message...
+                }
+        } );
+}
+
+sub history {
+    sleep(3);
+    print 'history(): ',
+        $p->history( {
+            channel => $channel
+        } ), 
+        "\n";
+}
