b32f1c26490d1f542ad91088b16679172b751c45
Stephen L. Blum <blum.stephen@gmail.com>
2011-02-02 20:36:13 -0800

    updated Corona SDK Multiplayer Networking and Push Notifications API Example Hello World Usage.

--- lua-corona/example-hello-world/main.lua
+++ lua-corona/example-hello-world/main.lua
@@ -1,9 +1,6 @@
 require "pubnub"
 
 multiplayer = pubnub.new({
-    --publish_key   = "pub-e1a3fb60-0952-4ec1-8906-52f10f868cf3",
-    --subscribe_key = "sub-24d15ae3-ebcb-11df-8fb9-1b433f1beafd",
-    --secret_key    = "sec-c01e7cc7-039a-4e07-8fdb-3af527c9f45a",
     publish_key   = "demo",
     subscribe_key = "demo",
     secret_key    = nil,
