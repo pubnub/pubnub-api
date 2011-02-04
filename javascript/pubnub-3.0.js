eff81c14624ecaaa91da14b4bb92bbeae3996229
Stephen L. Blum <blum.stephen@gmail.com>
2011-02-03 18:29:09 -0800

    updated PubNub JavaScript Cloud-Hosted Real-time Push API by adding new No-Confilct PUBNUB.clean() API and PUBNUB.init() API which offeres multi-account interfaces.

--- javascript/pubnub-3.0.js
+++ javascript/pubnub-3.0.js
@@ -1,33 +1,40 @@
-/*
-    WAIT! - This file depends on instructions from the PUBNUB Cloud.
+/* ---------------------------------------------------------------------------
+WAIT! - This file depends on instructions from the PUBNUB Cloud.
+http://www.pubnub.com/account-javascript-api-include
+--------------------------------------------------------------------------- */
 
-    http://www.pubnub.com/account-javascript-api-include
-*/
+/* ---------------------------------------------------------------------------
+PubNub Real-time Cloud-Hosted Push API and Push Notification Client Frameworks
+Copyright (c) 2011 TopMambo Inc.
+http://www.pubnub.com/
+http://www.pubnub.com/terms
+--------------------------------------------------------------------------- */
 
-/*
-    PubNub Real Time Push APIs and Notifications Framework
-    Copyright (c) 2010 Stephen Blum
-    http://www.google.com/profiles/blum.stephen
+/* ---------------------------------------------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+--------------------------------------------------------------------------- */
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/* =-=====================================================================-= */
-/* =-=====================================================================-= */
-/* =-==========================     JSON     =============================-= */
-/* =-=====================================================================-= */
-/* =-=====================================================================-= */
+/* =-====================================================================-= */
+/* =-====================================================================-= */
+/* =-=========================     JSON     =============================-= */
+/* =-====================================================================-= */
+/* =-====================================================================-= */
 
 (window['JSON'] && window['JSON']['stringify']) || (function () {
     window['JSON'] || (window['JSON'] = {});
@@ -184,11 +191,11 @@
 }());
 
 
-/* =-=====================================================================-= */
-/* =-=====================================================================-= */
-/* =-========================     DOM UTIL     ===========================-= */
-/* =-=====================================================================-= */
-/* =-=====================================================================-= */
+/* =-====================================================================-= */
+/* =-====================================================================-= */
+/* =-=======================     DOM UTIL     ===========================-= */
+/* =-====================================================================-= */
+/* =-====================================================================-= */
 
 window['PUBNUB'] || (function() {
 
@@ -513,212 +520,234 @@ function ajax( setup ) {
 }
 
 
-/* =-=====================================================================-= */
-/* =-=====================================================================-= */
-/* =-=========================     PUBNUB     ============================-= */
-/* =-=====================================================================-= */
-/* =-=====================================================================-= */
+/* =-====================================================================-= */
+/* =-====================================================================-= */
+/* =-=========================     PUBNUB     ===========================-= */
+/* =-====================================================================-= */
+/* =-====================================================================-= */
 
 var PN            = $('pubnub')
 ,   DEMO          = 'demo'
-,   PUBLISH_KEY   = attr( PN, 'pub-key' ) || DEMO
-,   SUBSCRIBE_KEY = attr( PN, 'sub-key' ) || DEMO
-,   SSL           = attr( PN, 'ssl' ) == 'on' ? 's' : ''
-,   ORIGIN        = 'http'+SSL+'://'+(attr(PN,'origin')||'pubsub.pubnub.com')
 ,   LIMIT         = 1800
 ,   READY         = 0
 ,   READY_BUFFER  = []
-,   CHANNELS      = {}
-,   PUBNUB        = {
-    /*
-        PUBNUB.history({
-            channel  : 'my_chat_channel',
-            limit    : 100,
-            callback : function(messages) { console.log(messages) }
-        });
-    */
-    'history' : function( args, callback ) {
-        var callback = args['callback'] || callback 
-        ,   limit    = args['limit'] || 100
-        ,   channel  = args['channel']
-        ,   jsonp    = jsonp_cb();
+,   CREATE_PUBNUB = function(setup) {
+    var CHANNELS      = {}
+    ,   PUBLISH_KEY   = setup['publish_key']   || DEMO
+    ,   SUBSCRIBE_KEY = setup['subscribe_key'] || DEMO
+    ,   SSL           = setup['ssl'] ? 's' : ''
+    ,   ORIGIN        = 'http'+SSL+'://'+(setup['origin']||'pubsub.pubnub.com')
+    ,   SELF          = {
+        /*
+            PUBNUB.history({
+                channel  : 'my_chat_channel',
+                limit    : 100,
+                callback : function(messages) { console.log(messages) }
+            });
+        */
+        'history' : function( args, callback ) {
+            var callback = args['callback'] || callback 
+            ,   limit    = args['limit'] || 100
+            ,   channel  = args['channel']
+            ,   jsonp    = jsonp_cb();
 
-        // Make sure we have a Channel
-        if (!channel)  return log('Missing Channel');
-        if (!callback) return log('Missing Callback');
+            // Make sure we have a Channel
+            if (!channel)  return log('Missing Channel');
+            if (!callback) return log('Missing Callback');
 
-        // Send Message
-        xdr({
-            callback : jsonp,
-            url      : [
-                ORIGIN, 'history',
-                SUBSCRIBE_KEY, encode(channel),
-                jsonp, limit
-            ],
-            success  : function(response) { callback(response) },
-            fail     : function(response) { log(response) }
-        });
-    },
-
-    /*
-        PUBNUB.time(function(time){ console.log(time) });
-    */
-    'time' : function(callback) {
-        var jsonp = jsonp_cb();
-        xdr({
-            callback : jsonp,
-            url      : [ORIGIN, 'time', jsonp],
-            success  : function(response) { callback(response[0]) },
-            fail     : function() { callback(0) }
-        });
-    },
-
-    /*
-        PUBNUB.uuid(function(uuid) { console.log(uuid) });
-    */
-    'uuid' : function(callback) {
-        var jsonp = jsonp_cb();
-        xdr({
-            callback : jsonp,
-            url      : [
-                'http' + SSL +
-                '://pubnub-prod.appspot.com/uuid?callback=' + jsonp
-            ],
-            success  : function(response) { callback(response[0]) },
-            fail     : function() { callback(0) }
-        });
-    },
-
-    /*
-        PUBNUB.publish({
-            channel : 'my_chat_channel',
-            message : 'hello!'
-        });
-    */
-    'publish' : function( args, callback ) {
-        var callback = callback || args['callback'] || function(){}
-        ,   message  = args['message']
-        ,   channel  = args['channel']
-        ,   jsonp    = jsonp_cb();
-
-        if (!message)     return log('Missing Message');
-        if (!channel)     return log('Missing Channel');
-        if (!PUBLISH_KEY) return log('Missing Publish Key');
-
-        // If trying to send Object
-        message = JSON['stringify'](message);
-
-        // Make sure message is small enough.
-        if (message.length > LIMIT) return log('Message Too Big');
-
-        // Send Message
-        xdr({
-            callback : jsonp,
-            success  : function(response) { callback(response) },
-            fail     : function() { callback([ 0, 'Disconnected' ]) },
-            url      : [
-                ORIGIN, 'publish',
-                PUBLISH_KEY, SUBSCRIBE_KEY,
-                0, encode(channel),
-                jsonp, encode(message)
-            ]
-        });
-    },
-
-    /*
-        PUBNUB.unsubscribe({ channel : 'my_chat' });
-    */
-    'unsubscribe' : function(args) {
-        var channel = args['channel'];
-
-        // Leave if there never was a channel.
-        if (!(channel in CHANNELS)) return;
-
-        // Disable Channel
-        CHANNELS[channel].connected = 0;
-
-        // Abort and Remove Script
-        CHANNELS[channel].done && 
-        CHANNELS[channel].done(0);
-    },
-
-    /*
-        PUBNUB.subscribe({
-            channel  : 'my_chat'
-            callback : function(message) { console.log(message) }
-        });
-    */
-    'subscribe' : function( args, callback ) {
-
-        // Reduce Status Flicker
-        if (!READY) return READY_BUFFER.push([ args, callback ]);
-
-        var channel   = args['channel']
-        ,   callback  = callback || args['callback']
-        ,   timetoken = 0
-        ,   error     = args['error'] || function(){};
-
-        // Make sure we have a Channel
-        if (!channel)       return log('Missing Channel');
-        if (!callback)      return log('Missing Callback');
-        if (!SUBSCRIBE_KEY) return log('Missing Subscribe Key');
-
-        if (!(channel in CHANNELS)) CHANNELS[channel] = {};
-
-        // Make sure we have a Channel
-        if (CHANNELS[channel].connected) return log('Already Connected');
-            CHANNELS[channel].connected = 1;
-
-        // Recurse Subscribe
-        function pubnub() {
-            var jsonp = jsonp_cb();
-
-            // Stop Connection
-            if (!CHANNELS[channel].connected) return;
-
-            // Connect to PubNub Subscribe Servers
-            CHANNELS[channel].done = xdr({
+            // Send Message
+            xdr({
                 callback : jsonp,
                 url      : [
-                    ORIGIN, 'subscribe',
+                    ORIGIN, 'history',
                     SUBSCRIBE_KEY, encode(channel),
-                    jsonp, timetoken
+                    jsonp, limit
                 ],
-                fail     : function() { timeout( pubnub, 1000 ); error()  },
-                success  : function(message) {
-                    if (!CHANNELS[channel].connected) return;
-                    timetoken = message[1];
-                    timeout( pubnub, 10 );
-                    each( message[0], function(msg) { callback(msg) } );
-                }
+                success  : function(response) { callback(response) },
+                fail     : function(response) { log(response) }
             });
-        }
+        },
 
-        // Begin Recursive Subscribe
-        pubnub();
-    },
+        /*
+            PUBNUB.time(function(time){ console.log(time) });
+        */
+        'time' : function(callback) {
+            var jsonp = jsonp_cb();
+            xdr({
+                callback : jsonp,
+                url      : [ORIGIN, 'time', jsonp],
+                success  : function(response) { callback(response[0]) },
+                fail     : function() { callback(0) }
+            });
+        },
 
-    // Expose PUBNUB Functions
-    'xdr'      : xdr,
-    'each'     : each,
-    'map'      : map,
-    'css'      : css,
-    '$'        : $,
-    'create'   : create,
-    'bind'     : bind,
-    'supplant' : supplant,
-    'head'     : head,
-    'search'   : search,
-    'attr'     : attr,
-    'now'      : unique
-};
+        /*
+            PUBNUB.uuid(function(uuid) { console.log(uuid) });
+        */
+        'uuid' : function(callback) {
+            var jsonp = jsonp_cb();
+            xdr({
+                callback : jsonp,
+                url      : [
+                    'http' + SSL +
+                    '://pubnub-prod.appspot.com/uuid?callback=' + jsonp
+                ],
+                success  : function(response) { callback(response[0]) },
+                fail     : function() { callback(0) }
+            });
+        },
 
-// Bind for PUBNUB.ready
+        /*
+            PUBNUB.publish({
+                channel : 'my_chat_channel',
+                message : 'hello!'
+            });
+        */
+        'publish' : function( args, callback ) {
+            var callback = callback || args['callback'] || function(){}
+            ,   message  = args['message']
+            ,   channel  = args['channel']
+            ,   jsonp    = jsonp_cb();
+
+            if (!message)     return log('Missing Message');
+            if (!channel)     return log('Missing Channel');
+            if (!PUBLISH_KEY) return log('Missing Publish Key');
+
+            // If trying to send Object
+            message = JSON['stringify'](message);
+
+            // Make sure message is small enough.
+            if (message.length > LIMIT) return log('Message Too Big');
+
+            // Send Message
+            xdr({
+                callback : jsonp,
+                success  : function(response) { callback(response) },
+                fail     : function() { callback([ 0, 'Disconnected' ]) },
+                url      : [
+                    ORIGIN, 'publish',
+                    PUBLISH_KEY, SUBSCRIBE_KEY,
+                    0, encode(channel),
+                    jsonp, encode(message)
+                ]
+            });
+        },
+
+        /*
+            PUBNUB.unsubscribe({ channel : 'my_chat' });
+        */
+        'unsubscribe' : function(args) {
+            var channel = args['channel'];
+
+            // Leave if there never was a channel.
+            if (!(channel in CHANNELS)) return;
+
+            // Disable Channel
+            CHANNELS[channel].connected = 0;
+
+            // Abort and Remove Script
+            CHANNELS[channel].done && 
+            CHANNELS[channel].done(0);
+        },
+
+        /*
+            PUBNUB.subscribe({
+                channel  : 'my_chat'
+                callback : function(message) { console.log(message) }
+            });
+        */
+        'subscribe' : function( args, callback ) {
+
+            // Reduce Status Flicker
+            if (!READY) return READY_BUFFER.push([ args, callback, SELF ]);
+
+            var channel   = args['channel']
+            ,   callback  = callback || args['callback']
+            ,   timetoken = 0
+            ,   error     = args['error'] || function(){};
+
+            // Make sure we have a Channel
+            if (!channel)       return log('Missing Channel');
+            if (!callback)      return log('Missing Callback');
+            if (!SUBSCRIBE_KEY) return log('Missing Subscribe Key');
+
+            if (!(channel in CHANNELS)) CHANNELS[channel] = {};
+
+            // Make sure we have a Channel
+            if (CHANNELS[channel].connected) return log('Already Connected');
+                CHANNELS[channel].connected = 1;
+
+            // Recurse Subscribe
+            function pubnub() {
+                var jsonp = jsonp_cb();
+
+                // Stop Connection
+                if (!CHANNELS[channel].connected) return;
+
+                // Connect to PubNub Subscribe Servers
+                CHANNELS[channel].done = xdr({
+                    callback : jsonp,
+                    url      : [
+                        ORIGIN, 'subscribe',
+                        SUBSCRIBE_KEY, encode(channel),
+                        jsonp, timetoken
+                    ],
+                    fail : function() { timeout( pubnub, 1000 ); error()  },
+                    success  : function(message) {
+                        if (!CHANNELS[channel].connected) return;
+                        timetoken = message[1];
+                        timeout( pubnub, 10 );
+                        each( message[0], function(msg) { callback(msg) } );
+                    }
+                });
+            }
+
+            // Begin Recursive Subscribe
+            pubnub();
+        },
+
+        /*
+            // A Clean No Conflict API (Remove PUBNUB Global)
+            PUBNUB.clean(true)(function(PUBNUB){
+                PUBNUB.subscribe({...})
+                PUBNUB.publish({...})
+            });
+        */
+        'clean' : function(remove) {
+            return function(cbready) {
+                cbready(SELF);
+                if (remove) delete window['PUBNUB'];
+            };
+        },
+
+        // Expose PUBNUB Functions
+        'each'     : each,
+        'map'      : map,
+        'css'      : css,
+        '$'        : $,
+        'create'   : create,
+        'bind'     : bind,
+        'supplant' : supplant,
+        'head'     : head,
+        'search'   : search,
+        'attr'     : attr,
+        'now'      : unique,
+        'init'     : CREATE_PUBNUB
+    };
+
+    return SELF;
+},
+PUBNUB = CREATE_PUBNUB({
+    'publish_key'   : attr( PN, 'pub-key' ),
+    'subscribe_key' : attr( PN, 'sub-key' ),
+    'ssl'           : attr( PN, 'ssl' ) == 'on',
+    'origin'        : attr( PN, 'origin' )
+});
+
+// Bind for PUBNUB Readiness to Subscribe
 bind( 'load', window, function() { timeout( function() {
     READY = 1;
-    each( READY_BUFFER, function(sub) {
-        PUBNUB['subscribe']( sub[0], sub[1] );
-    } );
+    each( READY_BUFFER, function(sub){sub[2]['subscribe']( sub[0], sub[1] )} );
 }, 1000 ); } );
 
 // Provide Global Interfaces
