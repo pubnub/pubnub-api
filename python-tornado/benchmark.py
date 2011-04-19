f4e288266b7e9c58cd56f9264e9c823444d11ee4
Stephen L. Blum <blum.stephen@gmail.com>
2011-04-18 18:19:48 -0700

    added Python Tornado Benchmark script for Round Trip Performance Testing between PubNub Cloud and Python Server.

---
+++ python-tornado/benchmark.py
@@ -0,0 +1,70 @@
+## www.pubnub.com - PubNub Real-time push service in the cloud. 
+# coding=utf8
+
+## PubNub Real-time Push APIs and Notifications Framework
+## Copyright (c) 2010 Stephen Blum
+## http://www.pubnub.com/
+
+## -----------------------------------
+## PubNub 3.0 Real-time Push Cloud API
+## -----------------------------------
+
+from Pubnub import Pubnub
+import sys
+import datetime
+import tornado
+
+publish_key   = len(sys.argv) > 1 and sys.argv[1] or 'demo'
+subscribe_key = len(sys.argv) > 2 and sys.argv[2] or 'demo'
+secret_key    = len(sys.argv) > 3 and sys.argv[3] or None
+ssl_on        = len(sys.argv) > 4 and bool(sys.argv[4]) or False
+
+## -----------------------------------------------------------------------
+## Initiat Class
+## -----------------------------------------------------------------------
+pubnub = Pubnub( publish_key, subscribe_key, secret_key, ssl_on )
+crazy  = ' ~`!@#$%^&*(顶顅Ȓ)+=[]\\{}|;\':",./<>?abcd'
+
+## -----------------------------------------------------------------------
+## Subscribe Example
+## -----------------------------------------------------------------------
+
+def connected() :
+    pubnub.publish({
+        'channel' : crazy,
+        'message' : { 'Info' : 'Connected!' }
+    })
+
+SUP = { 'max' : 0 }
+def TMP(message):
+
+    key = str(datetime.datetime.now())[11:19]
+
+    if not SUP.has_key(key) :
+        SUP[key] = 0
+
+    SUP[key] = SUP[key] + 1
+
+    if SUP[key] > SUP['max'] :
+        SUP['max'] = SUP[key]
+
+    print(message)
+    pubnub.publish({
+        'channel' : crazy,
+        'message' : key +
+            " Current: " +
+            str(SUP[key]) +
+            " MAX Trips per Second: " +
+            str(SUP['max'])
+    })
+    
+pubnub.subscribe({
+    'channel'  : crazy,
+    'connect'  : connected,
+    'callback' : TMP
+})
+
+## -----------------------------------------------------------------------
+## IO Event Loop
+## -----------------------------------------------------------------------
+tornado.ioloop.IOLoop.instance().start()
