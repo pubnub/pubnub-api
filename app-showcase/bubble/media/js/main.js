e3243fe5da6f43cbd9bab181155a5fa078c5d4fc
Phil Deschaine <deschai5@gmail.com>
2011-11-02 17:52:34 -0700

    capable of making more than one bubble now

--- app-showcase/bubble/media/js/main.js
+++ app-showcase/bubble/media/js/main.js
@@ -1,3 +1,4 @@
+var bubbles = [];
 var placed = false;
 
 var large_circle = new Path.Circle([676, 433], 100);
@@ -18,34 +19,40 @@ group.visible = false;
 
 function onMouseMove(event) {
   if (placed === false) {
-    group.children[0].position = event.point;
-    group.position = large_circle.bounds.center;  
+    for (var i = 0; i < bubbles.length; i++) {
+      bubbles[i].children[0].position = event.point;
+      bubbles[i].position = large_circle.bounds.center;  
+    }
   }
 }
 
 function onFrame(event) {
-  if (placed === false) {
-    group.children[0].rotate(1);
-  }
-  else {
-    group.scale(.99); 
+  for (var i = 0; i < bubbles.length; i++) {
+    if (placed === false) {
+      bubbles[i].children[0].rotate(1);
+    }
+    else {
+      bubbles[i].scale(.99); 
+    }
   }
 }
 
 function onMouseDown(event) {
   
-  if (placed === true) {
-    hit_result = group.hitTest(event.point);
-    if ((hit_result !== null) && (hit_result !== undefined)) {
-      group.scale(1.5);
-    }
-  } 
+  for (var i = 0; i < bubbles.length; i++) {
+    if (placed === true) {
+      hit_result = bubbles[i].hitTest(event.point);
+      if ((hit_result !== null) && (hit_result !== undefined)) {
+        bubbles[i].scale(1.5);
+      }
+    } 
 
-  if (placed === false) {
-    placed = true;
-    group.opacity = 1;
-    group.position = event.point;
-    group.dashArray = [14, 0];
+    if (placed === false) {
+      placed = true;
+      bubbles[i].opacity = 1;
+      bubbles[i].position = event.point;
+      bubbles[i].dashArray = [14, 0];
+    }
   }
 }
 
@@ -55,10 +62,15 @@ $("#place").click( function(e) {
 
   placed = false;
 
-  group.children[1].content = $("#bubble_text").val();
-  group.children[1].position = group.position;
-  group.visible = true;
-  group.children[0].dashArray = [14, 4];
-  group.opacity = .5;
+  var new_bubble = group.clone(); 
+  
+  new_bubble.children[1].content = $("#bubble_text").val();
+  new_bubble.children[1].position = new_bubble.position;
+  new_bubble.visible = true;
+  new_bubble.children[0].dashArray = [14, 4];
+  new_bubble.opacity = .5;
+
+  bubbles.push(new_bubble);
+
 });
 
