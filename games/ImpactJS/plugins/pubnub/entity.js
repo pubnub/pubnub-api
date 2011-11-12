ed364e534ffb8ac4c0c2fce28c1edd9f9eb1f8fc
Phil Deschaine <deschai5@gmail.com>
2011-11-11 16:12:05 -0800

    added plugins

---
+++ games/ImpactJS/plugins/pubnub/entity.js
@@ -0,0 +1,102 @@
+ig.module( 
+	'plugins.pubnub.entity'
+)
+.requires(
+	'impact.entity',	
+	'plugins.pubnub.game'
+)
+.defines(function(){
+
+
+ig.PubNubEntity = ig.Entity.extend({
+  counter: 0, 
+  update_every: 30,
+  belongs_to: "",
+  last_broadcasted: undefined, 
+
+  update: function() {
+    this.parent();
+    this.counter += 1;  
+    
+    var curr = { "pos": this.pos, "vel": this.vel }
+    if (this.checkIfUpdateNeeded(this.last_broadcasted, curr )) { 
+      this.broadcastUpdate();
+    }
+  },
+  
+  checkIfUpdateNeeded: function(prev, curr) {
+    // if we're not in a game, return false 
+    if (ig.game.game_status != "in_game")
+      return false;
+
+    // if it's not time for a new update, return false
+    if (this.counter % this.update_every != 0)  
+      return false;
+
+    // if this entity isn't under our control, return false
+    if (this.belongs_to != ig.game.which_player) 
+      return false;
+  
+    // update if we don't have a prev 
+    if (!prev) 
+      return true;
+
+    // if anything is different, return true
+    if (prev.pos.x != curr.pos.x) 
+      return true;
+    if (prev.pos.y != curr.pos.y) 
+      return true;
+    if (prev.vel.x != curr.vel.x) 
+      return true;
+    if (prev.vel.y != curr.vel.y) 
+      return true;
+
+    return false;
+  },
+
+
+  broadcastUpdate: function() {
+    to_send = {'type': 'ent_update', 
+               'pos': this.pos, 
+               'vel': this.vel, 
+               'id': this.id }
+    ig.game.event.fire("send_to_server", to_send);
+    this.last_broadcasted = { "pos": this.pos, "vel": this.vel } 
+
+  },
+
+  keepUpdated: function() {
+    var ent_obj = this;
+    ig.game.event.bind("got_from_server", function(message) {
+      if (message.id == ent_obj.id) {
+        switch (message.type) {
+          case "ent_update":
+            if (ent_obj.belongs_to != ig.game.which_player) {
+              ent_obj.pos = message.pos;
+              ent_obj.vel = message.vel;
+            }
+            break;
+
+          case "ent_now_yours":
+            ent_obj.belongs_to = ig.game.which_player;
+            ent_obj.pos = message.pos;
+            break;
+
+          case "ent_not_yours":
+            if (ig.game.which_player == "player_1") {
+              ent_obj.belongs_to = "player_2";
+            }
+            else {
+              ent_obj.belongs_to = "player_1"; 
+            }
+            ent_obj.pos = message.pos;
+            break;
+        }
+      }  
+    });
+  }
+
+});
+
+
+});
---
+++ games/ImpactJS/plugins/pubnub/game.js
@@ -0,0 +1,122 @@
+ig.module( 
+	'plugins.pubnub.game'
+)
+.requires(
+	'plugins.pubnub.lib',
+	'impact.game'
+)
+.defines(function(){
+
+  ig.PubNubGame = ig.Game.extend({
+
+    game_id : "",
+    player_id : "",
+    game_status : "not_in_game",
+    game_lobby : "pong_lobby",
+    player_notification : "",
+
+    init: function() {
+      var game_obj = this;
+      var p = PUBNUB;
+
+      // generate a unique player_id
+      p.uuid(function(uuid) {
+        game_obj.player_id = uuid;
+        console.log('generated uuid ' + uuid);
+
+        // listen on that channel
+        // this is how the server will send direct messages
+        p.subscribe({
+          channel  : uuid + "_from_server",
+          callback : function(message) { 
+            console.log('got_from_server ' + message.type + ' ' + message.id); 
+            game_obj.event.fire("got_from_server", message);
+          }
+        });
+
+        game_obj.event.bind("got_from_server", function(message) {
+          switch (message.type) {
+            case "in_queue":
+              console.log("in_queue");  
+              break;
+
+            case "game_found":
+              console.log("game_found");  
+              game_obj.which_player = message.which_player;
+              game_obj.getPuck().startMoving();
+              game_obj.game_status = "in_game";
+              if (game_obj.which_player == "player_1") {
+                game_obj.getPaddle2().keepUpdated();
+              }
+              else {
+                game_obj.getPaddle1().keepUpdated();
+              }
+              game_obj.getPuck().keepUpdated();
+              break;
+
+            case "still_there":
+              game_obj.event.fire("send_to_server", {'type':'still_here'});
+              break;
+
+            case "opponent_left":
+              console.log('opponent left');
+              break;
+
+            case "you_win":
+              game_obj.player_notification = 'You win!';
+              game_obj.getPuck().stopMoving(); 
+              break;
+
+            case "you_lose":
+              game_obj.player_notification = 'You lose!';
+              game_obj.getPuck().stopMoving(); 
+              break;
+
+            case "game_not_active":
+              break;
+          }
+        });
+        
+        setTimeout( function() {
+          game_obj.event.fire("send_to_lobby", {'type':'looking_for_game'}); 
+        }, 2000);
+
+      });
+
+      var event = this.event = {
+        list : {},
+        bind : function( name, fun ) {
+          //console.log( 'bind', name );
+          (event.list[name] = event.list[name] || []).push(fun);
+        },
+        fire : function( name, data ) {
+          //console.log( 'fire', name );
+          p.each(
+            event.list[name] || [],
+            function(fun) { fun(data) }
+          );
+        },
+      }
+
+      this.event.bind("send_to_lobby", function(message) {
+        console.log('sent_to_lobby ' + message.type + ' ' + message.id); 
+        message.player_id = game_obj.player_id;
+        p.publish({
+          channel : game_obj.game_lobby,
+          message : message 
+        });
+      });
+
+      this.event.bind("send_to_server", function(message) {
+        console.log('sent_to_server ' + message.type + ' ' + message.id); 
+        p.publish({
+          channel : game_obj.player_id + "_from_client",
+          message : message  
+        });
+      });
+
+    },
+
+  });
+
+});
