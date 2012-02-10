47b7da4d5b64b39a068a5bd0ec21bb80798d677b
Stephen Blum <blum.stephen@gmail.com>
2012-02-09 18:52:39 -0800

    added PubNub Cryptography in JavaScript.

---
+++ app-showcase/crypto/encrypt-pubnub.js
@@ -0,0 +1,39 @@
+// =====================================================================
+// STANFORD CRYPTO Library with AES Encryption
+// =====================================================================
+PUBNUB.secure = (function(){
+    var cipher_key = '';
+
+    function encrypt(data) {
+        return data && sjcl.encrypt(
+            cipher_key, JSON.stringify(data)
+        ) || data;
+    }
+
+    function decrypt(data) {
+        try { return JSON.parse(
+            sjcl.decrypt( cipher_key, data )
+        ); }
+        catch(e) { return null; }
+    }
+
+    return function(setup) {
+        var pubnub = PUBNUB.init(setup);
+        cipher_key = setup.cipher_key;
+
+        return {
+            publish : function(args) {
+                args.message = encrypt(args.message);
+                return pubnub.publish(args);
+            },
+            subscribe : function(args) {
+                var callback = args.callback;
+                args.callback = function(message) {
+                    callback(decrypt(message));
+                }
+
+                return pubnub.subscribe(args);
+            }
+        };
+    };
+})();
