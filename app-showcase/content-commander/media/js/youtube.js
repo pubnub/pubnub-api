7e3e43ff5fe8a743f46b065534997d264ca7321a
Philip Deschaine <deschai5@gmail.com>
2012-03-26 21:39:43 -0700

    working youtube sharing

--- app-showcase/content-commander/media/js/youtube.js
+++ app-showcase/content-commander/media/js/youtube.js
@@ -1,25 +1,9 @@
-
-var tag = document.createElement('script');
-tag.src = "http://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
 PUBNUB.events.bind("youtube_link", function(data) {
   $("#content").remove();
   $("#viewer_box").append("<div id='content'></div>");
-
-  player = new YT.Player('content', {
-    height: '390',
-    width: '640',
-    videoId: data.video_id,
-    events: {
-      'onReady': onPlayerReady
-    }
-  });
+  var new_iframe = '<iframe id="youtube_player" width="300" height="168" src="http://www.youtube.com/embed/' + data.video_id + '?autoplay=1" frameborder="0"  allowfullscreen></iframe>';
+  $("#content").append(new_iframe);
 });
+
+
+
