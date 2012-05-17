7348de33d7de28e76545985ae85a5817ce72609a
priyanka <priyanka@ITS67.itshastra.local>
2012-05-17 14:04:11 +0530

    Implemented HMAC-SHA256, AES Encryption and support for UUID generation

---
+++ python-twisted/PubnubCrypto.py
@@ -0,0 +1,92 @@
+## www.pubnub.com - PubNub Real-time push service in the cloud. 
+# coding=utf8
+
+## PubNub Real-time Push APIs and Notifications Framework
+## Copyright (c) 2010 Stephen Blum
+## http://www.pubnub.com/
+
+## -----------------------------------
+## PubNub 3.0 Real-time Push Cloud API
+## -----------------------------------
+
+from Crypto.Cipher import AES
+from Crypto.Hash import MD5
+from base64 import encodestring, decodestring 
+import hashlib
+import hmac
+
+class PubnubCrypto() :
+    """
+    #**
+    #* PubnubCrypto
+    #*
+    #**
+
+    ## Initiat Class
+    pc = PubnubCrypto
+
+    """
+   
+    def pad( self, msg, block_size=16 ):
+        """
+        #**
+        #* pad
+        #*
+        #* pad the text to be encrypted
+        #* appends a padding character to the end of the String
+        #* until the string has block_size length
+        #* @return msg with padding.
+        #**
+        """
+        return msg + ((block_size - len(msg) % block_size) * chr(block_size - len(msg) % block_size))
+       
+    def depad( self, msg ):
+        """
+        #**
+        #* depad
+        #*
+        #* depad the decryptet message"
+        #* @return msg without padding.
+        #**
+        """
+        return msg[0:-ord(msg[-1])]
+
+    def getSecret( self, key ):
+        """
+        #**
+        #* getSecret
+        #*
+        #* hases the key to MD5
+        #* @return key in MD5 format
+        #**
+        """
+        return MD5.new(key).digest()
+
+    def encrypt( self, key, msg ):
+        """
+        #**
+        #* encrypt
+        #*
+        #* encrypts the message
+        #* @return message in encrypted format
+        #**
+        """
+        secret = self.getSecret(key)
+        Initial16bytes='0123456789012345'
+        cipher = AES.new(secret,AES.MODE_CBC,Initial16bytes)
+        return encodestring(cipher.encrypt(self.pad(msg)))
+    
+    def decrypt( self, key, msg ):
+        """
+        #**
+        #* decrypt
+        #*
+        #* decrypts the message
+        #* @return message in decryped format
+        #**
+        """
+        secret = self.getSecret(key)
+        Initial16bytes='0123456789012345'
+        cipher = AES.new(secret,AES.MODE_CBC,Initial16bytes)
+        return self.depad((cipher.decrypt(decodestring(msg))))
+    
--- python-twisted/unit-test.py
+++ python-twisted/unit-test.py
@@ -15,14 +15,15 @@ from twisted.internet import reactor
 
 publish_key   = len(sys.argv) > 1 and sys.argv[1] or 'demo'
 subscribe_key = len(sys.argv) > 2 and sys.argv[2] or 'demo'
-secret_key    = len(sys.argv) > 3 and sys.argv[3] or None
-ssl_on        = len(sys.argv) > 4 and bool(sys.argv[4]) or False
+secret_key    = len(sys.argv) > 3 and sys.argv[3] or 'demo'
+cipher_key    = len(sys.argv) > 4 and sys.argv[4] or 'demo'
+ssl_on        = len(sys.argv) > 5 and bool(sys.argv[5]) or False
 
 ## -----------------------------------------------------------------------
 ## Initiat Class
 ## -----------------------------------------------------------------------
-pubnub = Pubnub( publish_key, subscribe_key, secret_key, ssl_on )
-crazy  = ' ~`!@#$%^&*(顶顅Ȓ)+=[]\\{}|;\':",./<>?abcd'
+pubnub = Pubnub( publish_key, subscribe_key, secret_key, cipher_key, ssl_on )
+crazy  = 'hello_world'
 
 ## ---------------------------------------------------------------------------
 ## Unit Test Function
@@ -61,9 +62,7 @@ def publish_complete(info):
 
 pubnub.publish({
     'channel' : crazy,
-    'message' : {
-        'some_text' : 'Hello World!'
-    },
+    'message' :  {'one': 'Hello World! --> ɂ顶@#$%^&*()!', 'two': 'hello2'},
     'callback' : publish_complete
 })
 
