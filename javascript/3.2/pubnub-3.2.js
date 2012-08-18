24ab1b63a58bf1b37d7e165ee94aa20e5f40e7e0
Stephen Blum <blum.stephen@gmail.com>
2012-08-17 17:25:07 -0700

    added unsubscribe for "-pnpres" channels.

--- javascript/3.2/pubnub-3.2.js
+++ javascript/3.2/pubnub-3.2.js
@@ -235,15 +235,16 @@ var db = (function(){
 /**
  * UTIL LOCALS
  */
-var NOW    = 1
-,   SWF    = 'https://dh15atwfs066y.cloudfront.net/pubnub.swf'
-,   REPL   = /{([\w\-]+)}/g
-,   ASYNC  = 'async'
-,   URLBIT = '/'
-,   XHRTME = 310000
-,   SECOND = 1000
-,   UA     = navigator.userAgent
-,   XORIGN = UA.indexOf('MSIE 6') == -1;
+var NOW             = 1
+,   SWF             = 'https://dh15atwfs066y.cloudfront.net/pubnub.swf'
+,   REPL            = /{([\w\-]+)}/g
+,   ASYNC           = 'async'
+,   URLBIT          = '/'
+,   XHRTME          = 310000
+,   SECOND          = 1000
+,   PRESENCE_SUFFIX = '-pnpres'
+,   UA              = navigator.userAgent
+,   XORIGN          = UA.indexOf('MSIE 6') == -1;
 
 /**
  * NEXTORIGIN
@@ -738,17 +739,21 @@ var PDIV          = $('pubnub') || {}
             PUBNUB.unsubscribe({ channel : 'my_chat' });
         */
         'unsubscribe' : function(args) {
-            var channel = args['channel'];
+            // Unsubscribe from both the Channel and the Presence Channel
+            _unsubscribe(args['channel']);
+            _unsubscribe(args['channel'] + PRESENCE_SUFFIX);
 
-            // Leave if there never was a channel.
-            if (!(channel in CHANNELS)) return;
+            function _unsubscribe(channel) {
+                // Leave if there never was a channel.
+                if (!(channel in CHANNELS)) return;
 
-            // Disable Channel
-            CHANNELS[channel].connected = 0;
+                // Disable Channel
+                CHANNELS[channel].connected = 0;
 
-            // Abort and Remove Script
-            CHANNELS[channel].done && 
-            CHANNELS[channel].done(0);
+                // Abort and Remove Script
+                CHANNELS[channel].done && 
+                CHANNELS[channel].done(0);
+            }
         },
 
         /*
@@ -787,7 +792,7 @@ var PDIV          = $('pubnub') || {}
                 CHANNELS[channel].connected = 1;
 
             // Recurse Subscribe
-            function pubnub() {
+            function _connect() {
                 var jsonp = jsonp_cb();
 
                 // Stop Connection
@@ -849,21 +854,19 @@ var PDIV          = $('pubnub') || {}
                         } );
 
                         timeout( pubnub, 10 );
-                    },
-                    
+                    }
                 });
             }
 
+            // Presence Subscribe
+            if (args['presence']) SELF.subscribe({
+                channel  : args['channel'] + PRESENCE_SUFFIX,
+                callback : presence,
+                restore  : args['restore']
+            });
+
             // Begin Recursive Subscribe
-            pubnub();
-            
-            if (args['presence']) {
-                SELF.subscribe({
-                    channel: args['channel']+"-pnpres",
-                    callback: presence,
-                    restore: args['restore']
-                });
-            }
+            _connect();
         },
         'here_now' : function( args, callback ) {
             var callback = args['callback'] || callback 
