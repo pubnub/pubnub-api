a664b1ca9954a29a4a713cfdd460ebbb23bd8785
geremy cohen <geremy@pubnub.com>
2012-09-06 15:42:11 -0700

    Changing version numbers, adding detailedHistory to README

--- lua-corona/3.3/pubnub.lua
+++ lua-corona/3.3/pubnub.lua
@@ -6,7 +6,7 @@
 -- http://www.pubnub.com/
 
 -- -----------------------------------
--- PubNub 3.1 Real-time Push Cloud API
+-- PubNub 3.3 Real-time Push Cloud API
 -- -----------------------------------
 
 require "Json"
