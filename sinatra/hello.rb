75d5beabc41a507ac211fee0c999684344dfdc77
geremy cohen <geremy@pubnub.com>
2012-09-12 17:34:07 -0700

    sinatra pub/sub working

--- sinatra/hello.rb
+++ sinatra/hello.rb
@@ -18,10 +18,6 @@ def pub_out(message)
   halt message[0].to_s if message[0] != []
 end
 
-#callback = method(:my_callback)
-#pubnub = Pubnub.new(:subscribe_key => :demo, :publish_key => :demo)
-#pubnub.publish(:channel => :hello_world, :callback => callback, :message => "#{Time.now} - Ladies and Gentlemen, Sinatra!")
-
 get '/sub' do
   my_sub_callback = method(:sub_out)
 
@@ -29,9 +25,9 @@ get '/sub' do
   pubnub.subscribe(:channel => :hello_world, :callback => my_sub_callback)
 end
 
-get '/pub:message' do
+get '/pub/:message' do
   my_pub_callback = method(:pub_out)
 
   pubnub = Pubnub.new(:subscribe_key => :demo, :publish_key => :demo)
-  pubnub.publish(:channel => :hello_world, :callback => my_pub_callback, :message => "#{Time.now} - Sinatra says #{params[:message].to_s}!")
+  pubnub.publish(:channel => :hello_world, :callback => my_pub_callback, :message => "#{Time.now} - Sinatra says #{params[:message]}!")
 end
