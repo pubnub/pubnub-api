f2a539884b4f1b15025aaa7a593bf2b01fb2acf1
geremy cohen <geremy@pubnub.com>
2012-09-12 17:32:35 -0700

    WIP: pub demo

--- sinatra/hello.rb
+++ sinatra/hello.rb
@@ -8,7 +8,12 @@ def my_callback(message)
   end
 end
 
-def my_stream(message)
+def sub_out(message)
+  puts(message[0].to_s)
+  halt message[0].to_s if message[0] != []
+end
+
+def pub_out(message)
   puts(message[0].to_s)
   halt message[0].to_s if message[0] != []
 end
@@ -17,11 +22,16 @@ end
 #pubnub = Pubnub.new(:subscribe_key => :demo, :publish_key => :demo)
 #pubnub.publish(:channel => :hello_world, :callback => callback, :message => "#{Time.now} - Ladies and Gentlemen, Sinatra!")
 
-get '/streamer' do
-  my_out = method(:my_stream)
+get '/sub' do
+  my_sub_callback = method(:sub_out)
+
+  pubnub = Pubnub.new(:subscribe_key => :demo)
+  pubnub.subscribe(:channel => :hello_world, :callback => my_sub_callback)
+end
+
+get '/pub:message' do
+  my_pub_callback = method(:pub_out)
 
   pubnub = Pubnub.new(:subscribe_key => :demo, :publish_key => :demo)
-  pubnub.subscribe(:channel => :hello_world, :callback => my_out)
-
-
+  pubnub.publish(:channel => :hello_world, :callback => my_pub_callback, :message => "#{Time.now} - Sinatra says #{params[:message].to_s}!")
 end
