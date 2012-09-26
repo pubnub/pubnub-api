21b2bfc0cd16f7bb785340bccb4e4bb8172cb47b
geremy cohen <geremy@pubnub.com>
2012-09-26 00:40:39 -0700

    Added subscribe, unsubscribe, publish

--- qooxdo/3.3/custom/source/class/custom/Application.js
+++ qooxdo/3.3/custom/source/class/custom/Application.js
@@ -41,24 +41,59 @@ qx.Class.define("custom.Application",
                  -------------------------------------------------------------------------
                  */
 
+                var label1 = new qx.ui.basic.Label("Last Received Message").set({
+                    decorator:"main",
+                    font:new qx.bom.Font(28, ["Verdana", "sans-serif"])
+                });
+                this.getRoot().add(label1, {left:20, top:10});
+
                 // Create a button
-                var button1 = new qx.ui.form.Button("Publish", "custom/test.png");
+                var button2 = new qx.ui.form.Button("Publish a message!", "custom/test.png");
+                var button1 = new qx.ui.form.Button("Subscribe!", "custom/test.png");
+                var button3 = new qx.ui.form.Button("Un-Subscribe!", "custom/test.png");
+
 
                 // Document is the application root
                 var doc = this.getRoot();
 
                 // Add button to document at fixed coordinates
-                doc.add(button1, {left:100, top:50});
+                doc.add(button3, {left:300, top:50});
+                doc.add(button2, {left:125, top:50});
+                doc.add(button1, {left:0, top:50});
 
                 // Add an event listener
-                button1.addListener("execute", function (e) {
+                button3.addListener("execute", function (e) {
+
+                    pubnub.unsubscribe({
+                        channel:'hello_world'
+                    });
+
+                    label1.set({"value":"Unsubscribed."});
+
+                });
+
+                button2.addListener("execute", function (e) {
 
                     pubnub.publish({
-                        channel : "hello_world",
-                        message : "Hello from Qooxdo!"
+                        channel:"hello_world",
+                        message:"Hello from Qooxdo on " + new Date() + " !"
+                    });
+                });
+
+                button1.addListener("execute", function (e) {
+
+                    pubnub.subscribe({
+                        channel:'hello_world',
+                        connect:function () {
+                            label1.set({"value":"Subscribed."});
+                        },
+                        callback:function (message) {
+                            label1.set({"value":message});
+                        }
                     });
 
                 });
+
             }
         }
     });
