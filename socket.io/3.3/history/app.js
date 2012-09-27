2404af4d1812deff798ec6e4835722fdfeff89f7
geremy cohen <geremy@pubnub.com>
2012-09-27 15:20:16 -0700

    Made history example a bit more interactive.

--- socket.io/3.3/history/app.js
+++ socket.io/3.3/history/app.js
@@ -1,36 +1,51 @@
-(function(){
+(function () {
 
 // -----------------------------------------------------------------------
 // PUBNUB SETUP
 // -----------------------------------------------------------------------
-var my_user_data = { name : "John" };
-var pubnub_setup = {
-    user          : my_user_data,
-    channel       : 'bootstrap-app',
-    publish_key   : 'demo',
-    subscribe_key : 'demo',
-    custom_presence : false,
-    presence : false
-};
+    var my_user_data = { name:"John" };
+    var pubnub_setup = {
+        user:my_user_data,
+        channel:'bootstrap-app',
+        publish_key:'demo',
+        subscribe_key:'demo',
+        custom_presence:false,
+        presence:false
+    };
 
 // -----------------------------------------------------------------------
 // CREATE CONNECTION FOR USER EVENTS
 // -----------------------------------------------------------------------
-var socket = io.connect( 'http://pubsub.pubnub.com/history', pubnub_setup );
+    var socket = io.connect('http://pubsub.pubnub.com/history', pubnub_setup);
 
 
 // -----------------------------------------------------------------------
 // WAIT FOR A CONNECTION, then publish and get history
 // -----------------------------------------------------------------------
-socket.on( 'connect', function() {
-  console.log('connected!!!');
-  
-  socket.send( 'this is test data', function(message) { });
-  socket.send( 'this is test data 2', function(message) {
-    socket.history({'count': 10}, function(response) {
-      console.log(response);
+
+    socket.on('connect', function () {
+        console.log('connected!!!');
+        seedHistory();
     });
-  });
-});
+
+
+    function seedHistory() {
+
+        for (a = 0; a < 9; a++) {
+            socket.send('test data ' + a, function (message) {} );
+        }
+
+        getHistory(socket);
+    }
+
+    var getHistory = function() {
+        socket.history({'count':10}, function (response) {
+            console.log(response);
+        });
+    }
+
+    textDiv = document.getElementById('text');
+    textDiv.onclick = getHistory;
+
 
 })();
