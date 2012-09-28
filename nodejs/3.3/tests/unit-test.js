4d94d77e92db88a65be8f7a24c1a164c815875fc
geremy cohen <geremy@pubnub.com>
2012-09-27 18:15:19 -0700

    Checking for presence of attributes in here_now.

--- nodejs/3.3/tests/unit-test.js
+++ nodejs/3.3/tests/unit-test.js
@@ -7,199 +7,195 @@ nodeunit = require('nodeunit');
 channel = 'unit-test-pubnub-nodejs';
 
 pubnub = PUBNUB.init({
-  publish_key: 'demo',
-  subscribe_key: 'demo'
+    publish_key:'demo',
+    subscribe_key:'demo'
 });
 
-publish_dummy = function(channel, callback, message) {
-  if (callback === null) {
-    callback = function() {};
-  }
-  return pubnub.publish({
-    channel: channel,
-    message: message || { test: "test" },
-    callback: callback
-  });
-};
-
-publish_test = function(test) {
-  test.expect(2);
-  return publish_dummy(channel, function(response) {
-    test.ok(response[0] === 1);
-    test.ok(response[1] === "Sent");
-    test.done();
-  });
-};
-
-time_test = function(test) {
-  test.expect(1);
-  return pubnub.time(function(time) {
-    test.ok(time);
-    test.done();
-  });
-};
-
-uuid_test = function(test) {
-  test.expect(1);
-  return pubnub.uuid(function(uuid) {
-    test.ok(uuid);
-    test.done();
-  });
-};
-
-detailed_history_test_1 = function(test) {
-  test.expect(2);
-  return pubnub.detailedHistory({
-    count: 1,
-    channel: channel,
-    callback: function(messages) {
-      test.ok(messages);
-      test.ok(messages[0][0].test === "test");
-      test.done();
+publish_dummy = function (channel, callback, message) {
+    if (callback === null) {
+        callback = function () {
+        };
     }
-  });
+    return pubnub.publish({
+        channel:channel,
+        message:message || { test:"test" },
+        callback:callback
+    });
 };
 
-detailed_history_test_2 = function(test) {
-  var temp_channel = channel + '-' + pubnub.uuid();
-  test.expect(6);
-  publish_callback = function(response) {
-    test.ok(response[0] === 1);
-    test.ok(response[1] === "Sent");
-  }
-  publish_dummy(temp_channel, publish_callback , 'first');
-  publish_dummy(temp_channel, publish_callback , 'second');
-  return pubnub.detailedHistory({
-    count : 1,
-    channel: temp_channel,
-    callback: function(messages) {
-      test.ok(messages);
-      test.ok(messages[0][0] === "second");
-      test.done();
-    }
-  });
+publish_test = function (test) {
+    test.expect(2);
+    return publish_dummy(channel, function (response) {
+        test.ok(response[0] === 1);
+        test.ok(response[1] === "Sent");
+        test.done();
+    });
 };
 
-detailed_history_test_3 = function(test) {
-  var temp_channel = channel + '-' + pubnub.uuid();
-  test.expect(6);
-  publish_callback = function(response) {
-    test.ok(response[0] === 1);
-    test.ok(response[1] === "Sent");
-  }
-  publish_dummy(temp_channel, publish_callback , 'first');
-  publish_dummy(temp_channel, publish_callback , 'second');
-  return pubnub.detailedHistory({
-    count : 1,
-    channel: temp_channel,
-    reverse: 'true',
-    callback: function(messages) {
-      test.ok(messages);
-      test.ok(messages[0][0] === "first");
-      test.done();
-    }
-  });
+time_test = function (test) {
+    test.expect(1);
+    return pubnub.time(function (time) {
+        test.ok(time);
+        test.done();
+    });
 };
 
-
-history_test = function(test) {
-  test.expect(2);
-  return pubnub.history({
-    limit: 1,
-    channel: channel,
-    callback: function(messages) {
-      test.ok(messages);
-      test.ok(messages[0].test === "test");
-      test.done();
-    }
-  });
+uuid_test = function (test) {
+    test.expect(1);
+    return pubnub.uuid(function (uuid) {
+        test.ok(uuid);
+        test.done();
+    });
 };
 
-subscribe_test = function(test) {
-  var test_channel;
-  test_channel = 'channel-' + PUBNUB.unique();
-  test.expect(2);
-  return pubnub.subscribe({
-    channel: test_channel,
-    connect: function() {
-      return publish_dummy(test_channel);
-    },
-    callback: function(message) {
-      test.ok(message);
-      test.ok(message.test === "test");
-      test.done();
-      return {stop: true};
-    }
-  });
-};
-
-run_dummy_subscribe = function(channel) {
-  var pubnub = PUBNUB.init({
-    publish_key: 'demo',
-    subscribe_key: 'demo'
-  });
-  return pubnub.subscribe({
-    channel: channel,
-    connect: function() {
-      return {stop: true};
-    },
-    callback: function() {
-      return {stop: true};
-    }
-  });
-};
-
-presence_test = function(test) {
-  var test_channel;
-  test_channel = 'channel-' + PUBNUB.unique();
-  test.expect(3);
-  return pubnub.presence({
-    channel: test_channel,
-    connect: function() {
-      run_dummy_subscribe(test_channel);
-    },
-    callback: function(message) {
-      test.ok(message);
-      test.ok(message.action === "join");
-      test.ok(message.occupancy === 1);
-      test.done();
-      return {stop: true};
-    }
-  });
-};
-
-here_now_test = function(test) {
-  var test_channel;
-  test_channel = 'channel-' + PUBNUB.unique();
-  test.expect(2);
-  return pubnub.subscribe({
-    channel: test_channel,
-    connect: function() {
-      pubnub.here_now({
-        channel: test_channel,
-        callback: function(message) {
-          test.ok(message);
-          test.ok(message.occupancy === 1);
-          publish_dummy(test_channel);
+detailed_history_test_1 = function (test) {
+    test.expect(2);
+    return pubnub.detailedHistory({
+        count:1,
+        channel:channel,
+        callback:function (messages) {
+            test.ok(messages);
+            test.ok(messages[0][0].test === "test");
+            test.done();
         }
-      });
-    },
-    callback: function(message) {
-      test.done();
-      return {stop: true};
+    });
+};
+
+detailed_history_test_2 = function (test) {
+    var temp_channel = channel + '-' + pubnub.uuid();
+    test.expect(6);
+    publish_callback = function (response) {
+        test.ok(response[0] === 1);
+        test.ok(response[1] === "Sent");
     }
-  });
+    publish_dummy(temp_channel, publish_callback, 'first');
+    publish_dummy(temp_channel, publish_callback, 'second');
+    return pubnub.detailedHistory({
+        count:1,
+        channel:temp_channel,
+        callback:function (messages) {
+            test.ok(messages);
+            test.ok(messages[0][0] === "second");
+            test.done();
+        }
+    });
+};
+
+detailed_history_test_3 = function (test) {
+    var temp_channel = channel + '-' + pubnub.uuid();
+    test.expect(6);
+    publish_callback = function (response) {
+        test.ok(response[0] === 1);
+        test.ok(response[1] === "Sent");
+    }
+    publish_dummy(temp_channel, publish_callback, 'first');
+    publish_dummy(temp_channel, publish_callback, 'second');
+    return pubnub.detailedHistory({
+        count:1,
+        channel:temp_channel,
+        reverse:'true',
+        callback:function (messages) {
+            test.ok(messages);
+            test.ok(messages[0][0] === "first");
+            test.done();
+        }
+    });
+};
+
+
+history_test = function (test) {
+    test.expect(2);
+    return pubnub.history({
+        limit:1,
+        channel:channel,
+        callback:function (messages) {
+            test.ok(messages);
+            test.ok(messages[0].test === "test");
+            test.done();
+        }
+    });
+};
+
+subscribe_test = function (test) {
+    var test_channel;
+    test_channel = 'channel-' + PUBNUB.unique();
+    test.expect(2);
+    return pubnub.subscribe({
+        channel:test_channel,
+        connect:function () {
+            return publish_dummy(test_channel);
+        },
+        callback:function (message) {
+            test.ok(message);
+            test.ok(message.test === "test");
+            test.done();
+            return {stop:true};
+        }
+    });
+};
+
+run_dummy_subscribe = function (channel) {
+    var pubnub = PUBNUB.init({
+        publish_key:'demo',
+        subscribe_key:'demo'
+    });
+    return pubnub.subscribe({
+        channel:channel,
+        connect:function () {
+            return {stop:true};
+        },
+        callback:function () {
+            return {stop:true};
+        }
+    });
+};
+
+presence_test = function (test) {
+    var test_channel;
+    test_channel = 'channel-' + PUBNUB.unique();
+    test.expect(3);
+    return pubnub.presence({
+        channel:test_channel,
+        connect:function () {
+            run_dummy_subscribe(test_channel);
+        },
+        callback:function (message) {
+            test.ok(message);
+            test.ok(message.action === "join");
+            test.ok(message.occupancy === 1);
+            test.done();
+            return {stop:true};
+        }
+    });
+};
+
+here_now_test = function (test) {
+    var test_channel;
+    test_channel = 'channel-' + PUBNUB.unique();
+    test.expect(3);
+
+    pubnub.here_now({
+        channel:test_channel,
+        callback:function (message) {
+            test.notEqual(message, null);
+            test.equal(message.occupancy, 0);
+            test.equal(message.uuids.length, 0);
+            test.done();
+            return {stop:true };
+        }
+    });
+
 };
 
 module.exports = {
-  "Publish Test": publish_test,
-  "History Test": history_test,
-  "Time Test": time_test,
-  "UUID Test": uuid_test,
-  "Subscribe Test": subscribe_test,
-  "Presence Test": presence_test,
-  "Here Now Test": here_now_test,
-  "Detailed History Test 1": detailed_history_test_1,
-  "Detailed History Test 2": detailed_history_test_2,
-  "Detailed History Test 3": detailed_history_test_3
+//  "Publish Test": publish_test,
+//  "History Test": history_test,
+//  "Time Test": time_test,
+//  "UUID Test": uuid_test,
+//  "Subscribe Test": subscribe_test,
+//  "Presence Test": presence_test,
+    "Here Now Test":here_now_test,
+    "Detailed History Test 1":detailed_history_test_1,
+    "Detailed History Test 2":detailed_history_test_2,
+    "Detailed History Test 3":detailed_history_test_3
 };
