d2f0926b677354edc6a4665acf10c1156689e08d
geremy cohen <geremy@pubnub.com>
2012-11-16 15:33:50 -0800

    Adding delegates and push notification request

--- objective-c/3.3/Pubnub/AppDelegate.m
+++ objective-c/3.3/Pubnub/AppDelegate.m
@@ -18,9 +18,26 @@
 {
     NSLog(@"launching");
     // Override point for customization after application launch.
+    [self.window makeKeyAndVisible];
+    
+	// Let the device know we want to receive push notifications
+	[[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
     return YES;
+    
 }
-							
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+	NSLog(@"My token is: %@", deviceToken);
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	NSLog(@"Failed to get token, error: %@", error);
+}
+
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
