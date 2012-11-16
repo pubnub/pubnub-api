9618ed6efff61185cf779ca3aa84da53ed7f385a
Igor Tryus <igort@Mac-mini-Igor-Tryus.local>
2012-11-16 16:32:37 +0200

    started work on encrypting/decrypting started work on pubnub moc server

--- go/src/pubnub/pubnub_crypto.go
+++ go/src/pubnub/pubnub_crypto.go
@@ -1,13 +1,19 @@
 package pubnub
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/hmac"
+	"crypto/md5"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
 	"io"
 )
+
+var _IV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05}
 
 func GetHmacSha256(secret_key string, input string) string {
 	hmac_sha256 := hmac.New(sha256.New, []byte(secret_key))
@@ -26,4 +32,52 @@ func GenUUID() (string, error) {
 	uuid[4] = 0x40 // version 4 Pseudo Random, see page 7
 
 	return hex.EncodeToString(uuid), nil
+}
+
+func EncryptString(chipher_key string, message string) string {
+	block, _ := aes_cipher(chipher_key)
+
+	value := []byte(message)
+
+	stream := cipher.NewCTR(block, _IV)
+	stream.XORKeyStream(value, value)
+
+	return fmt.Sprintf("%s", encode(value))
+}
+
+func DecryptString(chipher_key string, message string) string { //need add error catching
+	block, _ := aes_cipher(chipher_key)
+	value, _ := decode([]byte(message))
+
+	stream := cipher.NewCTR(block, _IV)
+	stream.XORKeyStream(value, value)
+
+	return fmt.Sprintf("%s", value)
+}
+
+func aes_cipher(chipher_key string) (cipher.Block, error) {
+	hash := md5.New()
+	io.WriteString(hash, chipher_key)
+	block, err := aes.NewCipher(hash.Sum(nil))
+	if err != nil {
+		return nil, err
+	}
+	return block, nil
+}
+
+//Encodes a value using base64
+func encode(value []byte) []byte {
+	encoded := make([]byte, base64.StdEncoding.EncodedLen(len(value)))
+	base64.StdEncoding.Encode(encoded, value)
+	return encoded
+}
+
+//Decodes a value using base64 
+func decode(value []byte) ([]byte, error) {
+	decoded := make([]byte, base64.StdEncoding.DecodedLen(len(value)))
+	b, err := base64.StdEncoding.Decode(decoded, value)
+	if err != nil {
+		return nil, err
+	}
+	return decoded[:b], nil
 }
--- go/src/pubnub/pubnub_test.go
+++ go/src/pubnub/pubnub_test.go
@@ -1,11 +1,88 @@
 package pubnub
 
-import "testing"
+import (
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"testing"
+)
 
 func TestPubnubInit(t *testing.T) {
 	//test body
 }
 
+func timePubnubHandler(w http.ResponseWriter, req *http.Request) {
+	ioutil.ReadAll(req.Body)
+	w.Header().Set("Content-Length", "2")
+	io.WriteString(w, "OK")
+}
+
+func setupPubnubMockServer(t *testing.T) {
+	http.HandleFunc("/time/0", timePubnubHandler)
+	ln, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("failed to listen - %s", err.Error())
+	}
+	go func() {
+		err = http.Serve(ln, nil)
+		if err != nil {
+			t.Fatalf("failed to start HTTP server - %s", err.Error())
+		}
+	}()
+	addr = ln.Addr()
+}
+
 func TestGetTime(t *testing.T) {
-	//test body
+	//starter.Do(func() { setupMockServer(t) })
+
+	testPUBNUB := PubnubInit("demo", "demo", "", "", false)
+	//testPUBNUB.ORIGIN = "http://" + addr.String()
+
+	testchannel := make(chan []byte)
+	go testPUBNUB.GetTime(testchannel)
+
+	response := <-testchannel
+	if len(response) == 0 {
+		t.Fatalf("Failed to get server time: %s", response)
+	}
+}
+
+func TestPublish(t *testing.T) {
+	testPUBNUB := PubnubInit("demo", "demo", "", "", false)
+	publish_channel_1 := make(chan []byte)
+	publish_channel_2 := make(chan []byte)
+	var response []byte
+
+	go testPUBNUB.Publish("test-channel", "test", publish_channel_1)
+	for {
+		value, ok := <-publish_channel_1
+		if !ok {
+			break
+		}
+		response = append(response, value...)
+	}
+	if response[0] != '0' && response[0] != '1' {
+		t.Fatalf("Failed to publish message without chipher key and ssl: %s", response)
+	}
+	if response[0] == '0' {
+		t.Fatalf("Failed to publish message without chipher key and ssl: %s", response[1])
+	}
+
+	testPUBNUB.SSL = true
+	response = []byte{}
+	go testPUBNUB.Publish("test-channel", "test", publish_channel_2)
+	for {
+		value, ok := <-publish_channel_2
+		if !ok {
+			break
+		}
+		response = append(response, value...)
+	}
+	if response[0] != '0' && response[0] != '1' {
+		t.Fatalf("Failed to publish message with ssl and without chipher key: %s", response)
+	}
+	if response[0] == '0' {
+		t.Fatalf("Failed to publish message with ssl and without chipher key: %s", response[1])
+	}
 }
