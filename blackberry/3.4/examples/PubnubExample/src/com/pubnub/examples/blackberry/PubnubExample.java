a7b37c3694c9efe9b51ee7f24bbdc233b48f3aa6
Devendra <devendra@udgama.com>
2012-12-27 02:37:07 +0530

    adding blackberry example

---
+++ blackberry/3.4/examples/PubnubExample/src/com/pubnub/examples/blackberry/PubnubExample.java
@@ -0,0 +1,64 @@
+package com.pubnub.examples.blackberry;
+
+import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.Dialog;
+
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
+
+/**
+ * This class extends the UiApplication class, providing a
+ * graphical user interface.
+ */
+public class PubnubExample extends UiApplication
+{
+    /**
+     * Entry point for application
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args)
+    {
+    	Pubnub pubnub = new Pubnub("demo", "demo", "demo", false);
+
+
+    	pubnub.publish("hello_world1","I am blackberry client" , new Callback() {
+    		public void successCallback(String channel, Object message) {
+    			System.out.println(message.toString());
+			}
+
+			public void errorCallback(String channel, Object message) {
+				System.out.println(message.toString());
+			}
+
+    	});
+    	Pubnub.startHeartbeat(5000);
+        // Create a new instance of the application and make the currently
+        // running thread the application's event dispatch thread.
+        PubnubExample theApp = new PubnubExample();
+        theApp.enterEventDispatcher();
+    }
+
+
+    /**
+     * Creates a new PubnubExample object
+     */
+    public PubnubExample()
+    {
+        // Push a screen onto the UI stack for rendering.
+        pushScreen(new PubnubExampleScreen());
+    }
+
+    /**
+     * Presents a dialog to the user with a given message
+     *
+     * @param message
+     *            The text to display
+     */
+    public static void alertDialog(final String message) {
+        UiApplication.getUiApplication().invokeLater(new Runnable() {
+            public void run() {
+                Dialog.alert(message);
+            }
+        });
+    }
+}
