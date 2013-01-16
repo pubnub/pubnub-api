8da8ce14f0d416bb6ddb66428a4fc31c22fdc8a6
Rongchao Fan <rongchao@pubnub.com>
2013-01-16 10:56:44 +0800

    add data for toggled item

--- content-commander/js/instructor.js
+++ content-commander/js/instructor.js
@@ -8,8 +8,29 @@ $("#publish").click(function(){
   });
 });
 
+$("#youtube").data("name", "youtube");
+$("#flickr").data("name", "flickr");
+$("#soundcloud").data("name", "soundcloud");
+$("#vimeo").data("name", "vimeo");
+
+$(".media-option").data('toggled',false);
+
 $(".media-option").click(function(){
     $(this).toggleClass("active");
+    var state = $(this).data('toggled');   
+    $(this).data('toggled', !state); 
+    console.log($(this).data("name")+":"+$(this).data("toggled"));
+});
+
+$("#search").click(function(){
+    var selected = [];
+    $(".media-option").each(function(){
+        console.log($(this).data("name"));
+        if($(this).data("toggled")){
+            selected.push($(this).data("name"));    
+        }
+    });
+    console.log("selected is；"+selected);
 });
 
 
