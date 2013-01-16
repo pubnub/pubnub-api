abe5d798546cd56be41858c0b77e8d3a9507e2a9
Rongchao Fan <rongchao@pubnub.com>
2013-01-16 11:45:18 +0800

    add comment on instructor.js

--- content-commander/js/instructor.js
+++ content-commander/js/instructor.js
@@ -1,4 +1,5 @@
 (function(){
+//TODO: show code snippet
 
 // bind click handler on #publish button
 $("#publish").click(function(){
@@ -8,13 +9,17 @@ $("#publish").click(function(){
   });
 });
 
+// add name data on media-option button
 $("#youtube").data("name", "youtube");
 $("#flickr").data("name", "flickr");
 $("#soundcloud").data("name", "soundcloud");
 $("#vimeo").data("name", "vimeo");
 
+// initialize media-option buttion with toggled false
 $(".media-option").data('toggled',false);
 
+// bind click handler on #toggle button  
+// change appearence and update data
 $(".media-option").click(function(){
     $(this).toggleClass("active");
     var state = $(this).data('toggled');   
@@ -22,6 +27,7 @@ $(".media-option").click(function(){
     console.log($(this).data("name")+":"+$(this).data("toggled"));
 });
 
+// bind click handler on #search button
 $("#search").click(function(){
     var selected = [];
     $(".media-option").each(function(){
@@ -32,7 +38,5 @@ $("#search").click(function(){
     });
     console.log("selected is；"+selected);
 });
-
-
 
 })();
