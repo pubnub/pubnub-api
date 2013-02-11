3edbe720413a3dace3d8c6b1e83170d3aa418b2d
geremy <geremy@pubnub.com>
2013-02-11 14:19:04 -0800

    Removing unused methods.

--- ruby/3.3/app/models/blocking_pub.rb
+++ ruby/3.3/app/models/blocking_pub.rb
@@ -4,8 +4,8 @@ class BlockingPub < ActiveRecord::Base
 
 
   def self.publish!
-    url = PubnubRequest.encode(['publish', "demo", "demo", '0', "b", '0', "hello".to_json])
-    url = "http://tophatter.pubnub.com#{url}"
+    url = PubnubRequest.encode(['publish', "demo", "demo", '0', "b", '0', "hello there".to_json])
+    url = "http://pubsub.pubnub.com#{url}"
 
     begin
       response = HTTParty.get(url, :headers => {'V' => '3.3', 'User-Agent' => 'Ruby (tophatter)', 'Accept' => '*/*'}, :timeout => 1)
@@ -50,18 +50,6 @@ class BlockingPub < ActiveRecord::Base
       @callback = options[:callback]
     end
 
-    def package!(data)
-      self.response = data.respond_to?(:content) ? Yajl.load(data.content) : Yajl.load(data)
-      self.last_timetoken = self.timetoken
-      self.timetoken = self.response[1]
-      self
-    end
-
-    def url
-      uri = URI.parse('http://pubsub.pubnub.co' + PubnubRequest.encode([self.operation, self.subscribe_key, self.channel, '0', self.timetoken]))
-      uri.query = "uuid=#{self.session_uuid}"
-      uri
-    end
 
     def self.encode(request)
       '/' + request.map { |bit| bit.split('').map { |ch| ' ~`!@#$%^&*()+=[]\\{}|;\':",./<>?'.index(ch) ? '%' + ch.unpack('H2')[0].to_s.upcase : URI.encode(ch) }.join('') }.join('/')
