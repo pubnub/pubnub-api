48803355abb492f6dec154ab6688b9b78e970396
geremy <geremy@pubnub.com>
2013-02-13 12:09:33 -0800

    fixing publish encoding bug

--- java/3.4/src/com/pubnub/httpclient/HttpClientCore.java
+++ java/3.4/src/com/pubnub/httpclient/HttpClientCore.java
@@ -3,18 +3,11 @@ package com.pubnub.httpclient;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DecompressingHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.util.EntityUtils;
 
 import com.pubnub.api.PubnubException;
 
@@ -22,53 +15,37 @@ import com.pubnub.api.PubnubException;
 public class HttpClientCore extends HttpClient {
 	private int requestTimeout = 310000;
 	private int connectionTimeout = 5000;
-	private DefaultHttpClient defaultHttpClient;
-	private DecompressingHttpClient httpClient;
-    private HttpGet httpGet;
-    private HttpParams httpParams;
+	HttpURLConnection connection;
+
 
     public void abortCurrentRequest(){
-    	if (httpGet != null)
-    		httpGet.abort();
+
     }
-    
+
     private void init() {
-    	DefaultHttpRequestRetryHandler retryHandler = new DefaultHttpRequestRetryHandler(0, false);
-		defaultHttpClient = new DefaultHttpClient();
-	    defaultHttpClient.setHttpRequestRetryHandler(retryHandler);
-		httpClient = new DecompressingHttpClient(defaultHttpClient);
-	    httpParams = httpClient.getParams();
-	    HttpConnectionParams.setSoTimeout(httpParams, requestTimeout);
-	    HttpConnectionParams.setConnectionTimeout(httpParams, connectionTimeout);
-	    
-	    System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
-		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "error");
+    	HttpURLConnection.setFollowRedirects(true);
     }
 	public HttpClientCore() {
 		init();
 	}
-	
+
 	public HttpClientCore(int requestTimeout, int connTimeout) {
 		this();
 		this.setRequestTimeout(requestTimeout);
 		this.setConnectionTimeout(connTimeout);
-		HttpConnectionParams.setSoTimeout(httpParams, requestTimeout);
-		HttpConnectionParams.setConnectionTimeout(httpParams, connectionTimeout);
 	}
 
 	public HttpClientCore(int requestTimeout) {
 		this();
 		this.setRequestTimeout(requestTimeout);
-		HttpConnectionParams.setSoTimeout(httpParams, requestTimeout);
 	}
-	
+
 	public int getRequestTimeout() {
 		return requestTimeout;
 	}
 
 	public void setRequestTimeout(int requestTimeout) {
 		this.requestTimeout = requestTimeout;
-		HttpConnectionParams.setSoTimeout(httpParams, requestTimeout);
 	}
 
 	public int getConnectionTimeout() {
@@ -77,21 +54,20 @@ public class HttpClientCore extends HttpClient {
 
 	public void setConnectionTimeout(int connectionTimeout) {
 		this.connectionTimeout = connectionTimeout;
-		HttpConnectionParams.setConnectionTimeout(httpParams, connectionTimeout);
 	}
 
 	public boolean isRedirect(int rc) {
-		return (rc == HttpStatus.SC_MOVED_PERMANENTLY
-				|| rc == HttpStatus.SC_MOVED_TEMPORARILY
-				|| rc == HttpStatus.SC_SEE_OTHER || rc == HttpStatus.SC_TEMPORARY_REDIRECT);
+		return (rc == HttpURLConnection.HTTP_MOVED_PERM
+				|| rc == HttpURLConnection.HTTP_MOVED_TEMP
+				|| rc == HttpURLConnection.HTTP_SEE_OTHER  );
 	}
 
 	public boolean checkResponse(int rc) {
-		return (rc == HttpStatus.SC_OK || isRedirect(rc));
+		return (rc == HttpURLConnection.HTTP_OK || isRedirect(rc));
 	}
-	
+
 	public boolean checkResponseSuccess(int rc) {
-		return (rc == HttpStatus.SC_OK);
+		return (rc == HttpURLConnection.HTTP_OK);
 	}
 
 	private static String readInput(InputStream in) throws IOException {
@@ -113,14 +89,15 @@ public class HttpClientCore extends HttpClient {
 	}
 
 	public synchronized HttpResponse fetch(String url, Hashtable headers) throws IOException, PubnubException {
-		
-		httpGet = new HttpGet(url);
+		URL urlobj = new URL(url);
+		 connection = (HttpURLConnection) urlobj.openConnection();
+		 connection.setRequestMethod("GET");
 		if (_headers != null) {
 			Enumeration en = _headers.keys();
 			while (en.hasMoreElements()) {
 			String key = (String) en.nextElement();
 			String val = (String) _headers.get(key);
-			httpGet.addHeader(key, val);
+			connection.addRequestProperty(key, val);
 			}
 		}
 		if (headers != null) {
@@ -128,22 +105,22 @@ public class HttpClientCore extends HttpClient {
 			while (en.hasMoreElements()) {
 			String key = (String) en.nextElement();
 			String val = (String) headers.get(key);
-			httpGet.addHeader(key, val);
+			connection.addRequestProperty(key, val);
 			}
 		}
-		org.apache.http.HttpResponse response = httpClient.execute(httpGet);
-		HttpEntity entity = response.getEntity();
-        String page = readInput(entity.getContent());
-        EntityUtils.consume(response.getEntity());	
+		connection.setReadTimeout(requestTimeout);
+		connection.setConnectTimeout(connectionTimeout);
+		connection.connect();
 
-		return new HttpResponse(response.getStatusLine().getStatusCode(), page);
+		String page = readInput(connection.getInputStream());
+
+		return new HttpResponse(connection.getResponseCode(), page);
 	}
 
 	public boolean isOk(int rc) {
-		return (rc == HttpStatus.SC_OK );
+		return (rc == HttpURLConnection.HTTP_OK );
 	}
 	public void shutdown() {
-		defaultHttpClient.getConnectionManager().shutdown();
-		init();
+		connection.disconnect();
 	}
 }
