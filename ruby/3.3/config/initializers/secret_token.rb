da17fdb85b879eb679b843b52bb495b43d56f98c
geremy <geremy@pubnub.com>
2013-02-26 14:47:15 -0800

    moving ruby

--- ruby/3.3/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ruby2::Application.config.secret_token = 'c91965d4a303e1bdaf3954f9160d7717bc2fc4ddb9d0a1fdc40716bd85d2c7aef014f391f806992e3ac9f0b15bbe0e89bbd26a283a85481ecdc9aff6f3e70f37'
