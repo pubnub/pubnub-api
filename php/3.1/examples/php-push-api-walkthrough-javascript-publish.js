ebc561a2fc02d181d8759304f65dd2ed84d2daaf
geremy <geremy@pubnub.com>
2013-03-20 13:28:18 -0700

    moving repo

--- php/3.1/examples/php-push-api-walkthrough-javascript-publish.js
+++
@@ -1,4 +0,0 @@
-PUBNUB.publish({
-    channel : 'extra_cool_channel',
-    message : { 'some_var' : 'what up?' }
-});
--- php/3.1/examples/php-push-api-walkthrough-php-publish.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require('../Pubnub.php');
-
-    $pubnub = new Pubnub( 'demo', 'demo' );
-    $pubnub->publish(array(
-        'channel' => 'my_test_channel',
-        'message' => array( 'some_text' => 'hello!' )
-    ));
-?>
